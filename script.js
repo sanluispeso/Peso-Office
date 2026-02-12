4c901d0e72ed669fa954c7e0f80bbeaf5221b20d
sanluispeso <sanluispeso3201@gmail.com>
2026-02-12 11:07:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// ENTER SYSTEM BUTTON
+function enterSystem() {
+    alert("Welcome to PESO San Luis, Aurora!");
+    // Redirect sa dashboard page
+    window.location.href = "dashboard.html";
+}
+
+// NAVIGATION BUTTONS (About, Vision & Mission, Org Chart)
+function goPage(page) {
+    window.location.href = page;
+}
